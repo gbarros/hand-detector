dbfc0e5fcc53a4f11a3e0abe27daa907cc231edc
Gabriel Barros <gbarros@stone.com.br>
2016-11-16 08:10:43 -0200

    Automated save

--- work/run.sh
+++ work/run.sh
@@ -1,2 +1,2 @@
-opencv_traincascade -data haar2 -vec "../images/LetraA/merged_A.vec" -bg ../images/background/bg1.txt -numPos 850 -numNeg 1034 -numStages 20 -featureType HAAR -w 40 -h 40 -precalcIdxBufSize 7168 -precalcValBufSize 7168 -mode ALL
+opencv_traincascade -data haar -vec "../images/LetraA/merged_A.vec" -bg ../images/background/bg1.txt -numPos 850 -numNeg 1034 -numStages 20 -featureType HAAR -w 40 -h 40 -precalcIdxBufSize 7168 -precalcValBufSize 7168 -mode ALL
 
