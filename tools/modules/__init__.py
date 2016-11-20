4ea604bc5e71c29081ead32077e6f60a72931476
gbarros <gbbabarros@gmail.com>
2016-11-19 22:18:31 -0200

    Add dataset tools to the repo

---
+++ tools/modules/__init__.py
@@ -0,0 +1,11 @@
+""""
+    This is a open source set of tools used to create
+training sets for OpenCV Cascade Classifiers.
+    It is free of charge and provided as-is. You may use 
+for research and study purposes only.
+
+    @author: Gabriel Barros 
+    @author-email: gbbabarros@gmail.com
+    All Rigths Reserved (2016)
+""""
+
---
+++ tools/modules/meta_json.py
@@ -0,0 +1,66 @@
+"""
+    Sample JSON [meta data] utils
+"""
+import argparse
+import utils
+import sys
+
+
+def get_args_merge():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-j", "--json-file", type=str, required=True,
+                    help='json files to be merged', nargs='+')
+    ap.add_argument("-o", "--outfile", type=str, required=True,
+                    help='file to hold the result')
+    args, _ = ap.parse_known_args()
+    return vars(args)
+
+
+def get_args_clean():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-j", "--json-file", type=str, required=True,
+                    help='json files to be merged')
+    ap.add_argument("-o", "--outfile", type=str, required=True,
+                    help='file to hold the result')
+    args, _ = ap.parse_known_args()
+    return vars(args)
+
+
+def merge():
+    args = get_args_merge()
+    jsons = []
+    for j in args["json_file"]:
+        jsons.extend(utils.open_json(j))
+    utils.save_json(jsons, args["outfile"])
+
+
+def clean():
+    args = get_args_clean()
+    import os
+    full_path = os.path.abspath(args["json_file"])
+    folder = os.path.dirname(full_path)
+    all_imgs = utils.open_json(args["json_file"])
+    filtered = utils.filter_by_type(os.listdir(folder), "_c.jpg")
+
+    clean_json = []
+    for img in all_imgs:
+        for one in filtered:
+            if utils.equal(img["file_name"], one):
+                clean_json.append(img)
+                break
+
+    utils.save_json(clean_json, args["outfile"])
+
+
+def run():
+    ap = argparse.ArgumentParser(
+        description="Module for actions on metadata JSON files, choose one:")
+    ap.add_argument("function", choices=["merge", "clean"])
+    args, _ = ap.parse_known_args()
+
+    if args.function == "merge":
+        merge()
+    elif args.function == "clean":
+        clean()
+if __name__ == "__main__":
+    run()
